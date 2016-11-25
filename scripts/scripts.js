999b08324a9cbd7081525184a18a76ea430f74f5
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-11-25 15:50:58 -0500

    Installed babel and tested intial render of react

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,15 @@
-require('./module1.js');
-require('./module2.js');
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component{
+	render() {
+		return (
+			<h1> It worked!</h1>
+			);
+	}
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);
+
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,19 @@ module.exports = {
   context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./scripts/scripts.js",
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/, //<----anything that is a js file gets ran through the babel loader
+        exclude:  /(node_modules|bower_components)/,
+        loader:  'babel-loader',
+        query:  {
+          presets: ['react', 'es2015', 'stage-2', 'latest'],
+          plugins: [ 'check-es2015-constants', 'transform-es2015-block-scoping', 'transform-es2015-arrow-functions', 'react-html-attrs', 'transform-class-properties','transform-decorators']
+        }
+      }
+    ]
+  },
   output: {
     path: __dirname + "/scripts",
     filename: "scripts.min.js"
