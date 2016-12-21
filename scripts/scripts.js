56118cdcacbe5fde86247c5479d8574fb5d8822e
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-12-21 15:58:01 -0600

    added vendor directory with bootstrap-without jquery and animation dropdowns are now working for small window size

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
+import Bootstrap from "./vendor/bootstrap-without-jquery";
 import Layout from "./components/Layout";
 
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(
+	<Router history= {hashHistory}>
+		<Route path= "/" component = {Layout}>
+		</Route>
+	</Router>, app);
 
