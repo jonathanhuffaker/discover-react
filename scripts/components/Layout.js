a8ef0d5c1e72eacc8a7691932c6b986305d0bc97
Jon Huffaker <jonathanhuffaker@gmail.com>
2016-12-22 15:50:36 -0600

    video 7 completed but need to take another look at the last of the vid as mine is not reflecting what is on the video.  Also need to clean up html and move pieces into layout

--- scripts/components/Layout.js
+++ scripts/components/Layout.js
@@ -7,11 +7,13 @@ export default class Layout extends React.Component {
   }
 
   render() {
-    return (
+    const { history } = this.props;
+    console.log(history.isActive("archives"));
+      return (
       <div>
         <h1>KillerNews.net</h1>
         {this.props.children}
-        <Link to="archives" class="btn btn-danger">archives</Link>
+        <Link to="archives" activeClassName = "test" >archives</Link>
         <Link to="settings"><button class="btn btn-success">settings</button></Link>
         <button onClick={this.navigate.bind(this)}>featured</button>
       </div>
